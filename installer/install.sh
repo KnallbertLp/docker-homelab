e885d5d63f7c6e69354b7fcd27cb7a7b347d1244
KnallbertLp <KnallbertLp@users.noreply.github.com>
2020-09-25 16:33:10 +0200

    ufw rearranged

--- installer/install.sh
+++ installer/install.sh
@@ -59,10 +59,10 @@ sudo apt full-upgrade -y
 sudo apt autoremove -y
 
 sudo apt install ufw -y
+sudo ufw logging off 
+sudo ufw allow ssh
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-sudo ufw allow ssh
-sudo ufw logging off 
 sudo ufw enable
 
 sudo echo 'Setting up avahi network discovery ...'
